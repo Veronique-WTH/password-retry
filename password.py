5ff2fe135aeb10a801565ad858e529fa42c31a4d
Vera Huang <veronique1015@gmail.com>
2022-08-25 18:10:18 +0800

    change to while times > 0

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 password = 'a123456'
 times = 3
-while True:
+while times > 0:
     pwd = input('please input your password:')
     if pwd == password:
         print('log in sucessfully.')
-        break 
+        break
     else:
         times = times - 1
         print('Password is wrong! You can try', times,'times')
-        if times == 0:
-            break
+
+       
